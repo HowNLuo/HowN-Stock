15f30efa1c9001f6742e3a5d6cf5a057af167720
Win <luohown@mitake.com.tw>
2023-05-26 09:52:53 +0800

    建立首頁畫面、撰寫假資料

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { Stock } from './../core/interface/stock.interface';
+import { mockStocks } from '../shared/mock/stock.mock';
+
+declare var $: any;
 
 @Component({
   selector: 'app-home',
@@ -6,10 +12,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  stockData: {title:string, data: string[][]} = {title:'', data: []};
+  stockKeyword: string;
+  yearAndMonth: string;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.setCalander();
   }
 
+  /** 設定日期選擇欄位 */
+  setCalander() {
+    $('.datepicker').datepicker({
+      minViewMode: 'months',
+      format: 'yyyy-mm',
+      autoclose: true
+    });
+  }
+
+  onSearchClick() {
+    this.stockData = mockStocks;
+  }
 }
