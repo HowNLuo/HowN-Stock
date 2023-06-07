c34b914fb2da28fb028374a5521e7f9657f8ccd9
Win <luohown@mitake.com.tw>
2023-06-07 10:13:47 +0800

    移除bootstrap-datepicker 新增ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http'
+
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HeaderComponent } from './header/header.component';
 import { PortfoliosComponent } from './portfolios/portfolios.component';
 import { HomeComponent } from './home/home.component';
 import { StockInfoComponent } from './stock-info/stock-info.component';
 import { PortfolioComponent } from './portfolios/portfolio/portfolio.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http'
 import { UserStocksDetailComponent } from './user-stocks-detail/user-stocks-detail.component';
 
 @NgModule({
@@ -28,8 +30,9 @@ import { UserStocksDetailComponent } from './user-stocks-detail/user-stocks-deta
     FormsModule,
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    BsDatepickerModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
