afa30ff8b98d000d2e5f012ec04a5015005b31df
Win.Luo <Win.Luo@infotech.com.tw>
2023-11-14 13:43:24 +0800

    angular升級至版本16

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,13 +1,13 @@
 import { AuthService } from './../core/service/auth.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
 constructor(
   private authService: AuthService,
